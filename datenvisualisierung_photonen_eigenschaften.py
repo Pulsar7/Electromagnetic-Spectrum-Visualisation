ff5b3d6cdaf4d9a747e7df3fcdc03cb0f1c2122b
Pulsar <62395480+Pulsar7@users.noreply.github.com>
2021-10-30 17:31:41 +0200

    Update datenvisualisierung_photonen_eigenschaften.py

--- datenvisualisierung_photonen_eigenschaften.py
+++ datenvisualisierung_photonen_eigenschaften.py
@@ -5,34 +5,54 @@
 import os,csv
 import matplotlib.pyplot as plt
 
-def calculate(max_number):
-	dateipfad = "daten.csv"
-	data = {
-		'wellenlänge': {
-			'elements': [],
-			'text': "Wellenlänge in nm"
-		},
-		'energie': {
-			'elements': [],
-			'text': "Energie in eV"
-		},
-		'frequenz': {
-			'elements': [],
-			'text': "Frequenz in Hz"
-		},
+data = {
+	'wellenlänge': {
+		'elements': [],
+		'text': "Wellenlänge in nm"
+	},
+	'energie': {
+		'elements': [],
+		'text': "Energie in eV"
+	},
+	'frequenz': {
+		'elements': [],
+		'text': "Frequenz in Hz"
 	}
+}
+dateipfad = "data/daten.csv"
+calculated_data_dateipfad = "calculated/calculated.csv"
+
+def write_data(rows):
+	with open(calculated_data_dateipfad,'w') as csvfile:
+		csvwriter = csv.writer(csvfile)
+		csvwriter.writerows(rows)
+
+def calculate(max_number):
+	rows = [
+		[
+		'Name',
+		'Wellenlänge in nm',
+		'Energie in eV','Frequenz in THz'
+		]
+	]
 	with open(dateipfad, 'r') as r:
 	    reader = csv.reader(r)
 	    for row in reader:
 	    	wellenlänge = float(row[1])
-	    	frequenz = (c / (wellenlänge/1000000000))
+	    	frequenz = (c / (wellenlänge/(10**9)))
 	    	energie = (h_evs * frequenz)
 	    	data['wellenlänge']['elements'].append(wellenlänge)
 	    	data['energie']['elements'].append(energie)
 	    	data['frequenz']['elements'].append(frequenz)
-	for element in data:
-		print(f'\n{data[element]["text"]}')
-		print(f'{data[element]["elements"]}')
+	    	name = row[0]
+	    	row = [
+	    		name,
+	    		'{:.2f}'.format(wellenlänge),
+	    		'{:.2f}'.format(energie),
+	    		'{:.2f}'.format(frequenz/(10**12)),
+	    	]
+	    	rows.append(row)
+	write_data(rows)
 	draw(data)
 
 def draw(data):
@@ -56,12 +76,12 @@ def draw(data):
 		xlabel=data['wellenlänge']['text'],
 		ylabel=data['frequenz']['text']
 	)
-
 	plt.show()
+	data.clear()
 
 h_js = (6.67408*10**-11) #Js
 h_evs = (4.135667662*10**-15) #eV*s
-c = 299792458 #m/s
+c = 299792458 #m/s - Vakuum
 max_number = 400
 if (__name__ == '__main__'):
 	os.system("clear") #Linux
